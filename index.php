efd7f7df56f82fd323ad06fbdfa23eb60072e796
Chris Blow <unthinkingly@gmail.com>
2010-08-06 22:11:22 -0700

    removed sidebar.

--- index.php
+++ index.php
@@ -7,34 +7,6 @@
   <?php $has_person_name = get_post_meta($post->ID, "contact_name_wpcm_value", true); ?>
   <?php $has_email = get_post_meta($post->ID, "email_wpcm_value", true); ?>
 
-  <!-- ================== -->
-  <!-- = recent entries = -->
-  <!-- ================== -->
-
-  <h2>3 Recent Sustainable Food System Contacts </h2><a href="/wp-admin/post-new.php?custom-write-panel-id=1">Add to the Network!</a>
-  
-  <table>  
-  <?php query_posts('showposts=3'); ?>  
-  <?php $alt = ""; ?>
-  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-  <?php if ($alt == "") { $alt = " alt"; } else { $alt = ""; } ?>
-  
-      <!-- ================= -->
-      <!-- = Contact entry = -->
-      <!-- ================= -->
-  
-      <tr class="contact<?php echo $alt; ?>">
-      <td>
-        <a href="<?php the_permalink(); ?>"><?php echo get_post_meta($post->ID, "organization_name_wpcm_value", true); ?><span class="viewlocation"></a> <a target="_blank" id="location-link" href="http://maps.google.com/maps?f=q&amp;hl=en&amp;q=<?php echo get_post_meta($post->ID, "address_1_wpcm_value", true); ?>,<?php echo get_post_meta($post->ID, "city_wpcm_value", true); ?>,<?php echo get_post_meta($post->ID, "state_wpcm_value", true); ?>,<?php echo get_post_meta($post->ID, "zip_wpcm_value", true); ?>">(View Location)</a></span>
-      </td>
-      <td>
-        <a href="<?php the_permalink(); ?>"><?php echo get_post_meta($post->ID, "contact_name_wpcm_value", true); ?></a>
-      </td>
-      <!-- <td class="m-category"><?php the_category(' '); ?></td> -->
-    </tr>
-  <?php endwhile; ?> 
-  </table>
-
 <!-- ====================== -->
 <!-- = No posts condition = -->
 <!-- ====================== -->  
